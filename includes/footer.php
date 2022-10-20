3fe08fbf93eceec639eafea7ba61f8b645e1f4b6
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-10-20 09:44:51 -0600

    archivos creados con cada parte searada

---
+++ includes/footer.php
@@ -0,0 +1,95 @@
+
+<footer id="rs-footer" class="rs-footer footer-main-home">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 md-mb-20">
+                    <div class="footer-logo mb-51 md-mb-30">
+                        <a href="index.html"><img src="assets/images/logo.png" alt=""></a>
+                    </div>
+                    <div class="textwidget mb-33">
+                        Lorem ipsum dolor sit amet of the consect adcing eli seddo work eiusmod temp orings
+                    </div>
+                    <ul class="app-store">
+                        <li><a href="#"><img src="assets/images/footer/playstore.png" alt=""></a></li>
+                        <li><a href="#"><img src="assets/images/footer/appstore.png" alt=""></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 pl-76 md-pl-15 md-mb-10">
+                    <h3 class="footer-title">Contact Info</h3>
+                    <ul class="address-widget">
+                        <li>
+                            <i class="fi fi-rr-map-marker-home"></i>
+                            <div class="desc">503 Old Buffalo Street<br>
+                                #205 New York-3087
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fi fi-rr-phone-call"></i>
+                            <div class="desc">
+                                <a href="tel:(+1)123-4567890">(+1) 123-456-7890</a>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fi fi-rr-envelope-plus"></i>
+                            <div class="desc">
+                                <a href="mailto:infoname@gmail.com">infoname@gmail.com</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 pl-75 md-pl-15 md-mb-10">
+                    <h3 class="footer-title">Services</h3>
+                    <ul class="site-map">
+                        <li><a href="credit-education.html">Credit Education</a></li>
+                        <li><a href="business-credit.html">Business Credit</a></li>
+                        <li><a href="credit-consulting.html">Credit Consulting</a></li>
+                        <li><a href="finance-consulting.html">Finance Consulting</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <h3 class="footer-title">Subscribe Newsletter</h3>
+                    <p>
+                        <input type="email" name="email" placeholder="Your Email Address" required="">
+                        <input type="submit" value="Subscribe Now">
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">                    
+            <div class="row y-middle">
+                <div class="col-lg-6 md-mb-10 text-lg-end text-center order-last">
+                    <ul class="footer-social md-mb-7">  
+                        <li> 
+                            <a href="#" target="_blank"><span><i class="fa fa-facebook"></i></span></a> 
+                        </li>
+                        <li> 
+                            <a href="# " target="_blank"><span><i class="fa fa-twitter"></i></span></a> 
+                        </li>
+
+                        <li> 
+                            <a href="# " target="_blank"><span><i class="fa fa-pinterest-p"></i></span></a> 
+                        </li>
+                        <li> 
+                            <a href="# " target="_blank"><span><i class="fa fa-instagram"></i></span></a> 
+                        </li>                                  
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <div class="copyright text-lg-start text-center ">
+                        <p>Copyright © 2022 Solvency. Designed By <a href="https://devsdesign.com/" target="blank"> Devsdesign</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
+
+<!-- start scrollUp  -->
+<div id="scrollUp">
+    <i class="fa fa-angle-up"></i>
+</div>
+<!-- End scrollUp  -->
---
+++ includes/header.php
@@ -0,0 +1,300 @@
+<!--Full width header Start-->
+<div class="full-width-header">
+    <!--Header Start-->
+    <header id="rs-header" class="rs-header">
+        <!-- Toolbar Area Start -->
+        <div class="toolbar-area topbar-style1 hidden-md">
+            <div class="container">
+                <div class="row rs-vertical-middle">
+                    <div class="col-lg-7">
+                        <div class="toolbar-contact">
+                            <ul class="rs-contact-info">
+                                <li>
+                                    <i class="fi fi-rr-envelope-plus"></i>
+                                    <a href="mailto:support@devsdesign.net">support@devsdesign.net</a>
+                                </li>
+                                <li>
+                                    <i class="fi fi-rr-phone-call"></i>
+                                    <a href="tel:(+088)589-8745"> (+088) 589-8745</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="toolbar-sl-share">
+                            <ul class="clearfix">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa fa-vimeo"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Toolbar Area End -->
+
+        <!-- Menu Start -->
+        <div class="menu-area menu-sticky">
+            <div class="container">
+                <div class="row-table">
+                    <div class="col-cell header-logo">
+                        <div class="logo-area">
+                            <a href="index.html">
+                                <img class="normal-logo" src="assets/images/logo-dark.png" alt="logo">
+                                <img class="sticky-logo" src="assets/images/logo-dark.png" alt="logo">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-cell">
+                        <div class="rs-menu-area">
+                            <div class="main-menu">
+                                <nav class="rs-menu hidden-md">
+                                    <ul class="nav-menu onepage-menu">
+                                        <li class="current-menu-item"><a href="#rs-header">Home</a></li>
+                                        <li><a href="#rs-about">About</a> </li>
+                                        <li><a href="#rs-tab-main">Services</a> </li>
+                                        <li><a href="#rs-project">Portfolios</a> </li>
+                                        <li><a href="#rs-blog">Blog</a> </li>
+                                        <li><a href="#rs-faq">Contact</a> </li>
+                                    </ul> <!-- //.nav-menu -->
+                                </nav>
+                            </div> <!-- //.main-menu -->
+                        </div>
+                    </div>
+                    <div class="col-cell">
+                        <div class="expand-btn-inner">
+                            <ul>
+                                <li class="sidebarmenu-search">
+                                    <a class="hidden-xs rs-search" data-bs-toggle="modal" data-bs-target="#searchModal"
+                                        href="#">
+                                        <i class="fi fi-rr-search"></i>
+                                    </a>
+                                </li>
+                                <li class="btn-quote"><a class="quote-button" href="#">Get A Quote</a></li>
+                                <li class="humburger">
+                                    <a id="nav-expander" class="nav-expander bar" href="#">
+                                        <div class="bar">
+                                            <span class="dot1"></span>
+                                            <span class="dot2"></span>
+                                            <span class="dot3"></span>
+                                            <span class="dot4"></span>
+                                            <span class="dot5"></span>
+                                            <span class="dot6"></span>
+                                            <span class="dot7"></span>
+                                            <span class="dot8"></span>
+                                            <span class="dot9"></span>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Menu End -->
+
+        <!-- Canvas Menu start -->
+        <nav class="right_menu_togle menu-wrap-off  hidden-md">
+            <div class="close-btn">
+                <a id="nav-close" class="nav-close">
+                    <div class="line">
+                        <span class="line1"></span>
+                        <span class="line2"></span>
+                    </div>
+                </a>
+            </div>
+            <div class="rs-offcanvas-inner">
+                <div class="canvas-logo">
+                    <a href="index.html"><img src="assets/images/logo-dark.png" alt="logo"></a>
+                </div>
+                <div class="offcanvas-text">
+                    <p>Teknolab quisque placerat vitae lacus ut scelerisque. Fusce luctus odio ac nibh luctus, in
+                        porttitor theo lacus egestas etiusto odio data center.</p>
+                </div>
+                <div class="canvas-contact">
+                    <div class="address-area">
+                        <div class="address-list">
+                            <div class="info-icon">
+                                <i class="flaticon-location"></i>
+                            </div>
+                            <div class="info-content">
+                                <h4 class="title">Address</h4>
+                                <em>05 kandi BR. New York</em>
+                            </div>
+                        </div>
+                        <div class="address-list">
+                            <div class="info-icon">
+                                <i class="flaticon-email"></i>
+                            </div>
+                            <div class="info-content">
+                                <h4 class="title">Email</h4>
+                                <em><a href="mailto:Devsdesign">Devsdesign</a></em>
+                            </div>
+                        </div>
+                        <div class="address-list">
+                            <div class="info-icon">
+                                <i class="flaticon-call"></i>
+                            </div>
+                            <div class="info-content">
+                                <h4 class="title">Phone</h4>
+                                <em>+019988772</em>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="social">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Canvas Menu end -->
+
+        <!-- Canvas Mobile Menu start -->
+        <nav class="right_menu_togle mobile-navbar-menu" id="mobile-navbar-menu">
+            <div class="close-btn">
+                <a id="nav-close2" class="nav-close">
+                    <div class="line">
+                        <span class="line1"></span>
+                        <span class="line2"></span>
+                    </div>
+                </a>
+            </div>
+            <ul class="nav-menu">
+                <li class="menu-item-has-children current-menu-item">
+                    <a href="index.html">Home</a>
+                    <ul class="sub-menu">
+                        <li class="menu-item-has-children current-menu-item">
+                            <a href="#">Multipages</a>
+                            <ul class="sub-menu">
+                                <li class="active"><a href="index.html">Home One</a></li>
+                                <li><a href="index2.html">Home Two</a></li>
+                                <li><a href="index3.html">Home Three</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="#">Onepages</a>
+                            <ul class="sub-menu">
+                                <li><a href="onepage1.html">Onepage One</a></li>
+                                <li><a href="onepage2.html">Onepage Two</a></li>
+                                <li><a href="onepage3.html">Onepage Three</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="about-us.html">About Us</a>
+                </li>
+                <li class="menu-item-has-children">
+                    <a href="services.html">Services</a>
+                    <ul class="sub-menu">
+                        <li><a href="credit-repair.html">Credit Repair</a></li>
+                        <li><a href="credit-audit.html">Credit Audit</a></li>
+                        <li><a href="credit-education.html">Credit Education</a></li>
+                        <li><a href="business-credit.html">Business Credit</a></li>
+                        <li><a href="credit-consulting.html">Credit Consulting</a></li>
+                        <li><a href="finance-consulting.html">Finance Consulting</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children">
+                    <a href="#">Pages</a>
+                    <ul class="sub-menu">
+                        <li class="last-item menu-item-has-children">
+                            <a href="#">Our Team</a>
+                            <ul class="sub-menu">
+                                <li><a href="our-team.html">Our Team</a></li>
+                                <li><a href="team-single.html">Team Single</a> </li>
+                            </ul>
+                        </li>
+                        <li class="last-item menu-item-has-children">
+                            <a href="#">Case Studies</a>
+                            <ul class="sub-menu">
+                                <li><a href="case-studies1.html">Case Studies 1</a></li>
+                                <li><a href="case-studies2.html">Case Studies 2</a></li>
+                                <li><a href="case-studies3.html">Case Studies 3</a></li>
+                                <li><a href="case-studies4.html">Case Studies 4</a></li>
+                                <li><a href="case-studies5.html">Case Studies 5</a></li>
+                                <li><a href="case-studies-filter.html">Case Studies Filter</a></li>
+                                <li><a href="case-studies-single.html">Case Studies Single</a></li>
+                            </ul>
+                        </li>
+                        <li class="last-item menu-item-has-children">
+                            <a href="#">Shop</a>
+                            <ul class="sub-menu">
+                                <li><a href="shop.html">Shop</a></li>
+                                <li><a href="shop-single.html">Shop Single</a></li>
+                                <li><a href="cart.html">Cart</a></li>
+                                <li><a href="checkout.html">Checkout</a></li>
+                                <li><a href="my-account.html">My Account</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="pricing-plans.html">Pricing Plans</a></li>
+                        <li><a href="partners.html">Partners</a></li>
+                        <li><a href="appointment.html">Appointment</a></li>
+                        <li class="last-item menu-item-has-children">
+                            <a href="#">Careers</a>
+                            <ul class="sub-menu">
+                                <li><a href="careers.html">Careers</a></li>
+                                <li><a href="careers-single.html">Careers Single</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="faq.html">Faq</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children">
+                    <a href="#">Blog</a>
+                    <ul class="sub-menu">
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="blog-single.html"> Blog Single</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="contact.html">Contact</a>
+                </li>
+            </ul> <!-- //.nav-menu -->
+            <!-- //.nav-menu -->
+
+            <!-- //.nav-menu -->
+            <div class="canvas-contact">
+                <div class="address-area">
+                    <div class="address-list">
+                        <div class="info-icon">
+                            <i class="flaticon-location"></i>
+                        </div>
+                        <div class="info-content">
+                            <h4 class="title">Address</h4>
+                            <em>05 kandi BR. New York</em>
+                        </div>
+                    </div>
+                    <div class="address-list">
+                        <div class="info-icon">
+                            <i class="flaticon-email"></i>
+                        </div>
+                        <div class="info-content">
+                            <h4 class="title">Email</h4>
+                            <em><a href="mailto:Devsdesign">Devsdesign</a></em>
+                        </div>
+                    </div>
+                    <div class="address-list">
+                        <div class="info-icon">
+                            <i class="flaticon-call"></i>
+                        </div>
+                        <div class="info-content">
+                            <h4 class="title">Phone</h4>
+                            <em>+019988772</em>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- Canvas Menu end -->
+    </header>
+    <!--Header End-->
+</div>
+<!--Full width header End-->
